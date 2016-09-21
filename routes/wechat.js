d9af7196afc962b0890e5910164fae334c92d299
zhanyunzhi <540562332@qq.com>
2016-09-21 22:39:40 +0800

    加入微信模块

---
+++ routes/wechat.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+var wechatDao = require('../dao/wechatDao');
+
+/* 微信接口严重页面 需要正确响应微信发送的Token验证 */
+router.get('/interface', function(req, res, next) {
+    wechatDao.openAddIn(req, res, next);
+});
+
+
+module.exports = router;
+
